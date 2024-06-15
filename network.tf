bb9f8c2a11d6ea199ec12c164de89c5849d44c8b
Heavenly Ren <heavenlyren@gmail.com>
2024-06-15 20:49:35 +0700

    fix typo

--- network.tf
+++ network.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "aca" {
   name                 = "${local.resource_prefix}-aca-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  service_endpoints    = ["Microsoft.Keyvault"]
+  service_endpoints    = ["Microsoft.KeyVault"]
   delegation {
     name = "Microsoft.App/environments"
     service_delegation {
